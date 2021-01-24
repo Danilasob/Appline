9560110e498e088124d8eee8500781386eb0c7c2
Danilasob <Sobolev27f950@mail.ru>
2021-01-24 21:23:11 +0300

    Редакция 3 задания

--- Appline3/src/Home_task_number_3.java
+++ Appline3/src/Home_task_number_3.java
@@ -1,22 +1,19 @@
 public class Home_task_number_3 {
     public static void main(String[]args)
     {
-    int[]array = {10,20,30,40,50};
-
+    int[]array = {100,27,8,44,72};
     int i;
     int sum;
-    int last = array[array.length - 1];;
-    int mid =array[0] +(array[array.length - 1] - array[0])/2; //формула действует если длина массива нечетное!
+    int mid =array[((array.length - 1)+1)/2]; //формула действует если длина массива нечетное!
     //System.out.println("Значение последнего элемента в массиве до смены: "+ last);
     //System.out.println("Значение среднего элемента в массиве: "+ mid);
 
+     i = array[0];
+     array[0] = array[array.length - 1] ;      //смена первого и последнего элемента массива
+     array[array.length - 1] = i;
 
-     i = array[0];      //смена первого и последнего элемента массива
-     array[0] = last;
-     last = array[4];
-     array[4]= i;
 
-     //System.out.println("Значение последнего элемента в массиве после смены: "+ array[4]); //проверка операции смены
+     System.out.println("Значение последнего элемента в массиве после смены: "+ array[4]); //проверка операции смены
 
      sum = array[0] + mid;
      System.out.println("Сумма первого и среднего элемент: " + sum);
