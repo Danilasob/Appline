8d24dc8199889ad24594e1c40ad01f1744ffbbc9
Danilasob <Sobolev27f950@mail.ru>
2021-02-02 22:28:09 +0300

    Добавление 8 задания

---
+++ Appline8/src/HomeTaskNamber8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class HomeTaskNamber8 {
+    public static void main (String [] args){
+        double sum=0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите любое целое положительное число: ");
+        double positiveNumber = scan.nextDouble();
+        for(int i =0;i<positiveNumber;i++){
+            if (i%2!=0){
+                sum = sum +i;
+            }
+        }
+        System.out.println("Cумма всех нечетные чисел от 1 до введенного числа: " + sum);
+    }
+}
